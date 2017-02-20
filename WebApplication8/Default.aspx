b6227deeb73a5aceb9a98918235d204681e58803
Z6OHM <Z6OHM@H51301>
2017-02-20 12:55:18 +0100

    Add project files.

---
+++ WebApplication8/Default.aspx
@@ -0,0 +1,98 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebApplication8.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style2 {
+            width: 566px;
+        }
+        .auto-style3 {
+            width: 1211px;
+        }
+        .auto-style4 {
+            width: 566px;
+            height: 85px;
+        }
+        .auto-style5 {
+            width: 1211px;
+            height: 85px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <table style="width:100%;">
+            <tr>
+                <td class="auto-style4">
+                    <asp:TextBox ID="TextBox2" runat="server" Width="42px"></asp:TextBox>
+&nbsp;&nbsp;&nbsp;
+                    <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Add jobs" Width="74px" />
+                </td>
+                <td class="auto-style5">
+                    <br />
+                    <br />
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    Bus1 messages: <asp:Label ID="Label1" runat="server" Font-Names="Arial" Font-Size="X-Large" ForeColor="#3399FF" Text="0"></asp:Label>
+                    <br />
+                    Bus2 messages:
+                    <asp:Label ID="Label2" runat="server" Font-Names="Arial" Font-Size="X-Large" ForeColor="#0099FF" Text="0"></asp:Label>
+                </td>
+                <td class="auto-style3">Jobs being processed:
+                    <asp:Label ID="Label3" runat="server" Font-Names="Arial" Font-Size="X-Large" ForeColor="#0099FF" Text="0"></asp:Label>
+                </td>
+            </tr>
+            <tr>
+                <td class="auto-style2">
+                    <br />
+&nbsp;<asp:Button ID="Button4" runat="server" OnClick="Button4_Click" Text="Refresh" />
+                    </td>
+                <td class="auto-style3">
+                    &nbsp;</td>
+            </tr>
+        </table>
+    
+    </div>
+                    <asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:App1DBConnectionString %>" SelectCommand="SELECT * FROM [TEST] WHERE ([DTProcessEnded] IS NULL) ORDER BY [DTProcessStarted] DESC, [Remarks] DESC"></asp:SqlDataSource>
+                    <br />
+        <br />
+                    <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="False" CellPadding="4" DataSourceID="SqlDataSource1" ForeColor="#333333" GridLines="None" Width="1117px">
+                        <AlternatingRowStyle BackColor="White" />
+                        <Columns>
+                            <asp:BoundField DataField="Id" HeaderText="Id" SortExpression="Id" />
+                            <asp:BoundField DataField="Count" HeaderText="Count" SortExpression="Count" />
+                            <asp:BoundField DataField="Remarks" HeaderText="Remarks" SortExpression="Remarks" />
+                            <asp:BoundField DataField="JobSize" HeaderText="JobSize" SortExpression="JobSize" />
+                            <asp:BoundField DataField="UserId" HeaderText="UserId" SortExpression="UserId" />
+                            <asp:BoundField DataField="DTReceived" HeaderText="DTReceived" SortExpression="DTReceived" />
+                            <asp:BoundField DataField="DTProcessStarted" HeaderText="DTProcessStarted" SortExpression="DTProcessStarted" />
+                            <asp:BoundField DataField="DTProcessEnded" HeaderText="DTProcessEnded" SortExpression="DTProcessEnded" />
+                        </Columns>
+                        <EditRowStyle BackColor="#2461BF" />
+                        <FooterStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                        <HeaderStyle BackColor="#507CD1" Font-Bold="True" ForeColor="White" />
+                        <PagerStyle BackColor="#2461BF" ForeColor="White" HorizontalAlign="Center" />
+                        <RowStyle BackColor="#EFF3FB" />
+                        <SelectedRowStyle BackColor="#D1DDF1" Font-Bold="True" ForeColor="#333333" />
+                        <SortedAscendingCellStyle BackColor="#F5F7FB" />
+                        <SortedAscendingHeaderStyle BackColor="#6D95E1" />
+                        <SortedDescendingCellStyle BackColor="#E9EBEF" />
+                        <SortedDescendingHeaderStyle BackColor="#4870BE" />
+                    </asp:GridView>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <p>
+                    &nbsp;</p>
+    </form>
+</body>
+</html>
---
+++ WebApplication8/Default.aspx.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Queue;
+using Newtonsoft.Json;
+
+namespace WebApplication8
+{
+    public partial class Default : System.Web.UI.Page
+    {
+
+        private static QueueClient queueClient;
+
+
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                Button4_Click(sender,e);
+                //updateListbox();
+
+            }
+        }
+
+      
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            int value;
+            if (int.TryParse(TextBox2.Text,out value))
+            {
+                SendMessages(value);
+                Button4_Click(sender,e);
+            }
+        }
+
+        private static void SendMessages(int count)
+        {
+
+
+
+            List<BrokeredMessage> smalljobs = new List<BrokeredMessage>();
+            List<BrokeredMessage> bigjobs = new List<BrokeredMessage>();
+            Random random = new Random();
+
+            for (int i = 0; i < count; i++)
+            {
+                var g = Guid.NewGuid();
+
+                int x = random.Next(300);
+
+
+
+                InsertDB(g,x);
+
+                if (x > 240)
+                  bigjobs.Add(CreateSampleMessage(g, "Message number: " + i));
+                else
+                  smalljobs.Add(CreateSampleMessage(g, "Message number: " + i));
+
+            }
+            SendMessages("bus1",smalljobs);
+            SendMessages("bus2",bigjobs);
+        }
+
+        private static void InsertDB(Guid g, int jobsize)
+        {
+            string bus;
+            if (jobsize > 240)
+            {
+                bus = "bus2";
+            }
+            else
+            {
+                bus = "bus1";
+                jobsize = jobsize/5;
+            }
+
+
+
+            SqlDataSource s = new SqlDataSource();
+            s.ConnectionString = ConfigurationManager.ConnectionStrings["App1DBConnectionString"].ToString();
+            s.InsertCommand = @"Insert into [dbo].[TEST] (Id,Count,Remarks,JobSize,UserId,DTReceived) Values (@g,1,@remarks,@jobsize,@UserId,@DTnow)";
+
+            s.InsertParameters.Add("g", g.ToString());
+            s.InsertParameters.Add("remarks", bus);
+            s.InsertParameters.Add("jobsize", jobsize.ToString());
+            s.InsertParameters.Add("UserId", Guid.NewGuid().ToString());
+            s.InsertParameters.Add("DTNow", DbType.DateTime, DateTime.UtcNow.ToString());
+
+            s.Insert();
+        }
+        private static void SendMessages(string queuename, List<BrokeredMessage> smalljobs)
+        {
+            queueClient = QueueClient.Create(queuename);
+            foreach (BrokeredMessage message in smalljobs)
+            {
+                while (true)
+                {
+                    try
+                    {
+                        queueClient.Send(message);
+                    }
+                    catch (MessagingException e)
+                    {
+                        if (!e.IsTransient)
+                        {
+                            throw;
+                        }
+                        else
+                        {
+                            HandleTransientErrors(e);
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+
+       
+
+        public static List<string> ReceiveMessages()
+        {
+            
+            List<string> l = new List<string>();
+            
+            queueClient = QueueClient.Create("Bus1");
+          
+
+            //var messages = queueClient.ReceiveBatch(10);
+            //foreach (var message in messages)
+            //{
+            //  l.Add(string.Format("Message received: Id = {0}, Body = {1}", message.MessageId, message.GetBody<string>()));
+            //}
+               
+            //queueClient.Close();
+
+            return l;
+        }
+
+
+        private static void HandleTransientErrors(MessagingException e)
+        {
+            //If transient error/exception, let’s back-off for 2 seconds and retry 
+            Console.WriteLine(e.Message);
+            Console.WriteLine("Will retry sending the message in 2 seconds");
+            Thread.Sleep(2000);
+        }
+        private static BrokeredMessage CreateSampleMessage(Guid messageId, string messageBody)
+        {
+            BrokeredMessage message = new BrokeredMessage(messageBody);
+            message.MessageId = messageId.ToString();
+            return message;
+        }
+
+       
+
+       
+       
+
+        protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          
+        }
+
+       
+
+       
+
+        protected void Button5_Click(object sender, EventArgs e)
+        {
+            SendMessages(10);
+        }
+
+        protected void Button4_Click(object sender, EventArgs e)
+        {
+            GridView1.DataBind();
+            queueClient = QueueClient.Create("bus2");
+
+
+
+            NamespaceManager nsmgr = Microsoft.ServiceBus.NamespaceManager.CreateFromConnectionString(System.Configuration.ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"]);
+            long count1 = nsmgr.GetQueue("bus1").MessageCount;
+            long count2 = nsmgr.GetQueue("bus2").MessageCount;
+
+            Label1.Text = count1.ToString();
+            Label2.Text = count2.ToString();
+
+            var connectionString = ConfigurationManager.ConnectionStrings["App1DBConnectionString"].ToString();
+            SqlConnection sqlConnection1 = new SqlConnection(connectionString);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = sqlConnection1;
+            sqlConnection1.Open();
+            cmd.CommandText = "Select Count('Id') From [dbo].[TEST] where [DTProcessStarted] is not null and [DTProcessEnded] is null";
+            int tt = (int) cmd.ExecuteScalar();
+            Label3.Text = tt.ToString();
+        }
+
+        protected void Button5_Click1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
